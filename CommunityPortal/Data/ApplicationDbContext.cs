7571d196fa06e608c8334968f2fe76760e84a950
Zealot0150 <per.claeson@gmail.com>
2022-03-23 14:15:13 +0100

    Roller..

--- CommunityPortal/Data/ApplicationDbContext.cs
+++ CommunityPortal/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityPortal.Models;
 using IdentityServer4.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using System;
@@ -25,7 +26,32 @@ namespace CommunityPortal.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            // addera rättigheter...
+            string adminRoleId = Guid.NewGuid().ToString();
+            string moderatorRoleId = Guid.NewGuid().ToString();
+            string userRoleId = Guid.NewGuid().ToString();
+
+            string accountId = Guid.NewGuid().ToString();
+
+
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole { Id = adminRoleId, Name = "Admin", NormalizedName = "ADMIN" });
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole { Id = moderatorRoleId, Name = "Moderator", NormalizedName = "MODERATOR" });
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole { Id = userRoleId, Name = "User", NormalizedName = "USER" });
+
+            PasswordHasher<CommunityUser> passwordHasher = new PasswordHasher<CommunityUser>();
+
+            modelBuilder.Entity<CommunityUser>().HasData(new CommunityUser
+            {
+                Id = accountId,
+                Email = "a@b.com",
+                NormalizedEmail = "A@B.COM",
+                UserName = "a@b.com",
+                NormalizedUserName = "A@B.COM",
+                PasswordHash = passwordHasher.HashPassword(null, "1234"),
+            });
+
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string> { UserId = accountId, RoleId = userRoleId });
+
+
 
         }
 
