58a1417aa4364833612864799ed069a189dbf82a
andreasberg-se <95615001+andreasberg-se@users.noreply.github.com>
2022-03-24 16:02:15 +0100

    Add files via upload

--- CommunityPortal/Models/CommunityUser.cs
+++ CommunityPortal/Models/CommunityUser.cs
@@ -1,14 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CommunityPortal.Models
 {
     public class CommunityUser : IdentityUser
     {
-        List<DiscussionPost> DiscussionPosts { get; set; }
-
-        List<DiscussionGroup> DiscussionGroups { get; set; }
-
-
     }
 }
--- CommunityPortal/Models/PrivateMessage.cs
+++ CommunityPortal/Models/PrivateMessage.cs
@@ -1,6 +1,35 @@
-﻿namespace CommunityPortal.Models
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CommunityPortal.Models
 {
+
     public class PrivateMessage
     {
+        [Key]
+        public int PrivateMessageId { get; set; }
+
+        [Required]
+        [Display(Name = "Header")]
+        public string Heading { get; set; }
+
+        [Required]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+
+        [Required]
+        [Display(Name = "From")]
+        public string FromUserName { get; set; }
+        public CommunityUser FromUser { get; set; }
+
+        [Required]
+        [Display(Name = "To")]
+        public string ToUserName { get; set; }
+        public CommunityUser ToUser { get; set; }
+
+        [Required]
+        [Display(Name = "Date")]
+        public DateTime Date { get; set; }
     }
+
 }
