aab826c40506035d04b5d12df41ea7a522c1a1a0
andreasberg-se <95615001+andreasberg-se@users.noreply.github.com>
2022-03-25 09:17:52 +0100

    Add files via upload

--- CommunityPortal/Controllers/PrivateMessageController.cs
+++ CommunityPortal/Controllers/PrivateMessageController.cs
@@ -8,28 +8,28 @@ namespace CommunityPortal.Controllers
 {
 
     [Route("api/[controller]")]
-    public class CategoryController : Controller
+    public class PrivateMessageController : Controller
     {
-        private readonly CategoryService _categoryService;
+        private readonly PrivateMessageService _privateMessageService;
 
-        public CategoryController(CategoryService categoryService)
+        public PrivateMessageController(PrivateMessageService privateMessageService)
         {
-            _categoryService = categoryService;
+            _privateMessageService = privateMessageService;
         }
 
         [HttpGet]
         public string Get()
         {
-            List<Category> Categories = _categoryService.GetList();
-            string JsonData = JsonSerializer.Serialize(Categories);
+            List<PrivateMessage> PrivateMessages = _privateMessageService.GetList();
+            string JsonData = JsonSerializer.Serialize(PrivateMessages);
             return JsonData;
         }
 
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            Category category = _categoryService.GetById(id);
-            string JsonData = JsonSerializer.Serialize(category);
+            PrivateMessage privateMessage = _privateMessageService.GetById(id);
+            string JsonData = JsonSerializer.Serialize(privateMessage);
             return JsonData;
         }
 
