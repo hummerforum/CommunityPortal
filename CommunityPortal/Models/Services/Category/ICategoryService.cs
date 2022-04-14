b3f14f173c7ec15fd42668dd016fcc83fac98f15
andreasberg-se <95615001+andreasberg-se@users.noreply.github.com>
2022-04-14 09:19:13 +0200

    Add files via upload

--- CommunityPortal/Models/Services/Category/ICategoryService.cs
+++ CommunityPortal/Models/Services/Category/ICategoryService.cs
@@ -8,7 +8,6 @@ namespace CommunityPortal.Models.Services
         public Category Add(Category category);
         public Category GetById(int categoryId);
         public List<Category> GetList();
-        public List<Category> GetParentList();
         public Category Update(Category category);
         public bool Delete(int categoryId);
     }
