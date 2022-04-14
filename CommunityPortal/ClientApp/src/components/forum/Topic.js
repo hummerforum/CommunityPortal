9018e8423181e627dbebffeda55b07832aa60f6d
gyaru <gyaru@users.noreply.github.com>
2022-04-14 14:03:20 +0200

    add fancy colours based on username

--- CommunityPortal/ClientApp/src/components/forum/Topic.js
+++ CommunityPortal/ClientApp/src/components/forum/Topic.js
@@ -68,6 +68,19 @@ const ReplyDate = styled.h1`
   font-size: 12px;
 `;
 
+function getColour(string) {
+	var colors = ["#e51c23", "#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#5677fc", "#03a9f4", "#00bcd4", "#009688", "#259b24", "#8bc34a", "#afb42b", "#ff9800", "#ff5722", "#795548", "#607d8b"]
+	
+    var hash = 0;
+	if (string.length === 0) return hash;
+    for (var i = 0; i < string.length; i++) {
+        hash = string.charCodeAt(i) + ((hash << 5) - hash);
+        hash = hash & hash;
+    }
+    hash = ((hash % colors.length) + colors.length) % colors.length;
+    return colors[hash];
+}
+
 function SendPm(navigate, authorId, date, content) {
   let uri = `/messages/${authorId}/Reply%20to%20post%20posted%20at:%20${date}/Reply%20to%20message:%20${content.replace(
     /[/?.]/g,
@@ -84,7 +97,7 @@ function Reply(props) {
         <AuthorName>{props.authorName}</AuthorName>
         <Avatar
           alt={props.authorName}
-          sx={{ bgcolor: "#95B2B8", color: "#FFF", width: 150, height: 150 }}
+          sx={{ bgcolor: getColour(props.authorName), color: "#FFF", width: 150, height: 150, fontSize: 60 }}
         >
           {props.authorName.charAt(0)}
         </Avatar>
