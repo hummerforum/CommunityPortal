f8f672534d0562c8ec1992781a53872886a1e3a8
andreasberg-se <95615001+andreasberg-se@users.noreply.github.com>
2022-04-22 08:15:20 +0200

    Add files via upload

--- CommunityPortal/Data/ApplicationDbContext.cs
+++ CommunityPortal/Data/ApplicationDbContext.cs
@@ -136,12 +136,6 @@ namespace CommunityPortal.Data
             modelBuilder.Entity<IdentityUserRole<string>>().HasData(
                 new IdentityUserRole<string> { UserId = accountId3, RoleId = adminRoleId });
 
-            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
-                new IdentityUserRole<string> { UserId = accountId4, RoleId = userRoleId });
-
-            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
-                new IdentityUserRole<string> { UserId = accountId5, RoleId = userRoleId });
-
 
             // News  post and categories
 
@@ -229,14 +223,193 @@ namespace CommunityPortal.Data
                 Information = "info2"
             };
 
-
-
-
             modelBuilder.Entity<NewsPost>().HasData(np1);
             modelBuilder.Entity<NewsPost>().HasData(np2);
             modelBuilder.Entity<NewsPost>().HasData(np3);
             modelBuilder.Entity<NewsPost>().HasData(np4);
             modelBuilder.Entity<NewsPost>().HasData(np5);
+
+            modelBuilder.Entity<NewsPost>().HasData(new NewsPost()
+            {
+                NewsPostId = 6,
+                IsEvent = false,
+                CategoryId = 3,
+                UserName = cu.UserName,
+                CreatedDate = new DateTime(2022, 4, 1),
+                UpdatedDate = new DateTime(2022, 4, 1),
+                Heading = "Bad Joke 4",
+                Information = "Can a dog jump higher than a house? Well, duh. Houses can’t jump !",
+                Tag = "Tag",
+                Description =  ""
+            });
+
+            modelBuilder.Entity<NewsPost>().HasData(new NewsPost()
+            {
+                NewsPostId = 7,
+                IsEvent = false,
+                CategoryId = 1,
+                UserName = cu.UserName,
+                CreatedDate = new DateTime(2022, 4, 1),
+                UpdatedDate = new DateTime(2022, 4, 1),
+                Heading = "What is Lorem Ipsum?",
+                Information = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                Tag = "Tag",
+                Description = ""
+            });
+
+            modelBuilder.Entity<NewsPost>().HasData(new NewsPost()
+            {
+                NewsPostId = 8,
+                IsEvent = false,
+                CategoryId = 1,
+                UserName = cu.UserName,
+                CreatedDate = new DateTime(2022, 4, 1),
+                UpdatedDate = new DateTime(2022, 4, 1),
+                Heading = "Where does Lorem Ipsum come from?",
+                Information = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.. comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H.Rackham.",
+                Tag = "Tag",
+                Description = ""
+            });
+
+            modelBuilder.Entity<NewsPost>().HasData(new NewsPost()
+            {
+                NewsPostId = 9,
+                IsEvent = false,
+                CategoryId = 1,
+                UserName = cu.UserName,
+                CreatedDate = new DateTime(2022, 4, 1),
+                UpdatedDate = new DateTime(2022, 4, 1),
+                Heading = "Why do we use Lorem Ipsum?",
+                Information = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+                Tag = "Tag",
+                Description = ""
+            });
+
+            modelBuilder.Entity<NewsPost>().HasData(new NewsPost()
+            {
+                NewsPostId = 10,
+                IsEvent = false,
+                CategoryId = 1,
+                UserName = cu.UserName,
+                CreatedDate = new DateTime(2022, 4, 1),
+                UpdatedDate = new DateTime(2022, 4, 1),
+                Heading = "Where can I get some Lorem Ipsum?",
+                Information = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+                Tag = "Tag",
+                Description = ""
+            });
+
+            modelBuilder.Entity<NewsPost>().HasData(new NewsPost()
+            {
+                NewsPostId = 11,
+                IsEvent = false,
+                CategoryId = 2,
+                UserName = cu.UserName,
+                CreatedDate = new DateTime(2022, 4, 2),
+                UpdatedDate = new DateTime(2022, 4, 2),
+                Heading = "Perch",
+                Information = "Perch is a common name for fish of the genus Perca, freshwater gamefish belonging to the family Percidae. The perch, of which three species occur in different geographical areas, lend their name to a large order of vertebrates: the Perciformes, from the Greek: πέρκη (perke), simply meaning perch, and the Latin forma meaning shape. Many species of freshwater gamefish more or less resemble perch, but belong to different genera. In fact, the exclusively saltwater-dwelling red drum is often referred to as a red perch, though by definition perch are freshwater fish. Though many fish are referred to as perch as a common name, to be considered a true perch, the fish must be of the family Percidae.",
+                Tag = "Tag",
+                Description = ""
+            });
+
+            modelBuilder.Entity<NewsPost>().HasData(new NewsPost()
+            {
+                NewsPostId = 12,
+                IsEvent = false,
+                CategoryId = 2,
+                UserName = cu.UserName,
+                CreatedDate = new DateTime(2022, 4, 2),
+                UpdatedDate = new DateTime(2022, 4, 2),
+                Heading = "Pike",
+                Information = "The pike is a large fish that can grow over a metre in length. It is found in lakes and slow-flowing rivers and canals that have a lot of vegetation. It uses these plants as hiding places when hunting, bursting out with remarkable speed to catch fish, frogs, small mammals or ducklings. Young pike are called 'jack' and will eat small fish and invertebrates. Pike spawn between March and May, returning to the same place every year. A large female can produce up to 500,000 eggs.",
+                Tag = "Tag",
+                Description = ""
+            });
+
+            modelBuilder.Entity<NewsPost>().HasData(new NewsPost()
+            {
+                NewsPostId = 13,
+                IsEvent = false,
+                CategoryId = 2,
+                UserName = cu.UserName,
+                CreatedDate = new DateTime(2022, 4, 3),
+                UpdatedDate = new DateTime(2022, 4, 3),
+                Heading = "Salmon",
+                Information = "Salmon is the common name for several species of ray-finned fish in the family Salmonidae. Other fish in the same family include trout, char, grayling, and whitefish. Salmon are native to tributaries of the North Atlantic (genus Salmo) and Pacific Ocean (genus Oncorhynchus). Many species of salmon have been introduced into non-native environments such as the Great Lakes of North America and Patagonia in South America. Salmon are intensively farmed in many parts of the world.",
+                Tag = "Tag",
+                Description = ""
+            });
+
+            modelBuilder.Entity<NewsPost>().HasData(new NewsPost()
+            {
+                NewsPostId = 14,
+                IsEvent = false,
+                CategoryId = 2,
+                UserName = cu.UserName,
+                CreatedDate = new DateTime(2022, 4, 3),
+                UpdatedDate = new DateTime(2022, 4, 3),
+                Heading = "Lobster",
+                Information = "Lobsters are a family (Nephropidae, synonym Homaridae) of large marine crustaceans. Lobsters have long bodies with muscular tails, and live in crevices or burrows on the sea floor.Three of their five pairs of legs have claws including the first pair, which are usually much larger than the others.Highly prized as seafood, lobsters are economically important, and are often one of the most profitable commodities in coastal areas they populate. ",
+                Tag = "Tag",
+                Description = ""
+            });
+
+            modelBuilder.Entity<NewsPost>().HasData(new NewsPost()
+            {
+                NewsPostId = 15,
+                IsEvent = false,
+                CategoryId = 2,
+                UserName = cu.UserName,
+                CreatedDate = new DateTime(2022, 4, 4),
+                UpdatedDate = new DateTime(2022, 4, 4),
+                Heading = "Crab",
+                Information = "Crabs are decapod crustaceans of the infraorder Brachyura, which typically have a very short projecting tail (abdomen), usually hidden entirely under the thorax. They live in all the world's oceans, in fresh water, and on land, are generally covered with a thick exoskeleton, and have a single pair of pincers. They first appeared during the Jurassic Period.",
+                Tag = "Tag",
+                Description = ""
+            });
+
+            modelBuilder.Entity<NewsPost>().HasData(new NewsPost()
+            {
+                NewsPostId = 16,
+                IsEvent = false,
+                CategoryId = 1,
+                UserName = cu.UserName,
+                CreatedDate = new DateTime(2022, 4, 2),
+                UpdatedDate = new DateTime(2022, 4, 2),
+                Heading = "Microsoft to kill Patch Tuesday for some enterprise users",
+                Information = "The company plans to launch its Windows Autopatch service for enterprise users with Windows 10 or Windows 11 Enterprise E3 licenses; it will continuously deploy patches, eliminating the need for admins to manually deploy them.",
+                Tag = "Tag",
+                Description = ""
+            });
+
+            modelBuilder.Entity<NewsPost>().HasData(new NewsPost()
+            {
+                NewsPostId = 17,
+                IsEvent = false,
+                CategoryId = 1,
+                UserName = cu.UserName,
+                CreatedDate = new DateTime(2022, 4, 3),
+                UpdatedDate = new DateTime(2022, 4, 3),
+                Heading = "What will Apple announce at WWDC 2022?",
+                Information = "WWDC speculation has officially begun. So, what will Apple announce at its annual developer conference in June? Macworld executive editor Michael Simon and Computerworld executive editor Ken Mingis join Juliet to discuss what to expect at WWDC this year, including updates to Apple’s operating systems like iOS and macOS and maybe even some hardware announcements.",
+                Tag = "Tag",
+                Description = ""
+            });
+
+            /*modelBuilder.Entity<NewsPost>().HasData(new NewsPost()
+            {
+                NewsPostId = ,
+                IsEvent = false,
+                CategoryId = 1,
+                UserName = cu.UserName,
+                CreatedDate = new DateTime(2022, 4, 1),
+                UpdatedDate = new DateTime(2022, 4, 1),
+                Heading = ".",
+                Information = ".",
+                Tag = "Tag",
+                Description = ""
+            });*/
 
 
             // set up forum
